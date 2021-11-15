ac21a931451a2f47840e3d751e931dc47d0a1511
Eskil Brännerud <eskilb@kth.se>
2021-11-15 18:56:51 +0100

    added 3 sites

---
+++ src/components/settingsPage/settingsPage.js
@@ -0,0 +1,18 @@
+class Application extends React.Component {
+    render() {
+      return (
+        
+        <Container fluid="lg">
+          <Row className="justify-content-md-center mt-5">
+            <Col className="d-grid" md="8">
+              <Button variant="primary" size="lg">
+                Inne i Settings
+             </Button>
+            </Col>
+          </Row>
+        </Container>
+      );
+    }
+  }
+  
+  export default Application;
