d61a11c95a2d2647c9397f7d7d7efe06bf9b4de5
Eskil Brännerud <eskilb@kth.se>
2021-11-15 18:36:13 +0100

    basic menu page

--- src/components/menuPage/menuPage.js
+++ src/components/menuPage/menuPage.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 
 class Application extends React.Component {
   render() {
     return (
-      <div className="App">
-        <Button onClick={() => toggleShow(true)}>Show Toast</Button>
-      </div>
+      
+      <Container fluid="lg">
+        <Row className="justify-content-md-center mt-5">
+          <Col className="d-grid" md="8">
+            <Button variant="primary" size="lg">
+              Analys
+           </Button>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center mt-5">
+          <Col className="d-grid" md="8">
+            <Button variant="primary" size="lg">
+              Kassa
+           </Button>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center mt-5">
+          <Col className="d-grid" md="8">
+            <Button variant="primary" size="lg">
+              Depå
+           </Button>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center mt-5">
+          <Col className="d-grid" md="8">
+            <Button variant="primary" size="lg">
+              Inställningar
+           </Button>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
